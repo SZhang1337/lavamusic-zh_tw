3c82edf4cedf40cd68c178c21374ab1135e261fd
AZhang阿樟 <54375883+SZhang1337@users.noreply.github.com>
2022-08-13 01:23:41 +0800

    Update join.js
    zh_tw version of join.js
    
    Signed-off-by: AZhang阿樟 <54375883+SZhang1337@users.noreply.github.com>

--- src/commands/Music/join.js
+++ src/commands/Music/join.js
@@ -16,13 +16,13 @@ module.exports = {
     
     let player = message.client.manager.get(message.guildId);
         if(player && player.voiceChannel && player.state === "CONNECTED") {
-            return await message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription( `I'm already connected to <#${player.voiceChannel}> voice channel!`)]})
+            return await message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription( `我已經在 <#${player.voiceChannel}> 裡了!`)]})
         } else {
-    if (!message.guild.members.me.permissions.has([PermissionFlagsBits.Connect, PermissionFlagsBits.Speak])) return message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`I don't have enough permissions to execute this command! Please give me permission \`CONNECT\` or \`SPEAK\`.`)]});
+    if (!message.guild.members.me.permissions.has([PermissionFlagsBits.Connect, PermissionFlagsBits.Speak])) return message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`我沒有足夠的權限來使用此指令! 煩請給我 \`連線\` 或 \`講話\`權限。`)]});
 
     const { channel } = message.member.voice;
    
-    if (!message.guild.members.me.permissionsIn(channel).has([PermissionFlagsBits.Connect, PermissionFlagsBits.Speak])) return message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`I don't have enough permissions to connect to your VC. Please give me permission \`CONNECT\` or \`SPEAK\`.`)]});
+    if (!message.guild.members.me.permissionsIn(channel).has([PermissionFlagsBits.Connect, PermissionFlagsBits.Speak])) return message.channel.send({embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`我沒有足夠的權限來進入語音頻道。 煩請給我 \`連線\` 以及 \`講話\`權限。`)]});
    
     const emojiJoin = message.client.emoji.join;
 
@@ -37,7 +37,7 @@ module.exports = {
 
       let thing = new EmbedBuilder()
         .setColor(client.embedColor)
-        .setDescription(`${emojiJoin} **Joined the voice channel**\nJoined <#${channel.id}> and bound to <#${message.channel.id}>`)
+        .setDescription(`${emojiJoin} **已加入語音頻道**\n已加入 <#${channel.id}> 並綁定頻道至 <#${message.channel.id}>`)
       return message.reply({ embeds: [thing] });
 
     };
