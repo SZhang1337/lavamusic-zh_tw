b14bacb125e1a4d07e5773b11a56e19225cd393b
AZhang阿樟 <54375883+SZhang1337@users.noreply.github.com>
2022-08-13 16:45:40 +0800

    Update play.js
    zh_tw version of play.js
    
    Signed-off-by: AZhang阿樟 <54375883+SZhang1337@users.noreply.github.com>

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -15,9 +15,9 @@ module.exports = {
   sameVoiceChannel: true,
   execute: async (message, args, client, prefix) => {
 
-    if (!message.guild.members.me.permissions.has(PermissionsBitField.resolve(['Speak', 'Connect']))) return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`I don't have enough permissions to execute this command! please give me permission \`CONNECT\` or \`SPEAK\`.`)] });
+    if (!message.guild.members.me.permissions.has(PermissionsBitField.resolve(['Speak', 'Connect']))) return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`我沒有足夠的權限來行使這個權限! 請給我 \`連線\` 或 \`講話\`權限。`)] });
     const { channel } = message.member.voice;
-    if (!message.guild.members.cache.get(client.user.id).permissionsIn(channel).has(PermissionsBitField.resolve(['Speak', 'Connect']))) return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`I don't have enough permissions connect your vc please give me permission \`CONNECT\` or \`SPEAK\`.`)] });
+    if (!message.guild.members.cache.get(client.user.id).permissionsIn(channel).has(PermissionsBitField.resolve(['Speak', 'Connect']))) return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setDescription(`我沒有足夠的權限來進入語音! 請給我 \`連線\` 或 \`講話\`權限。`)] });
 
     
     const emojiaddsong = message.client.emoji.addsong;
@@ -38,7 +38,7 @@ module.exports = {
     try {
       res = await player.search(search, message.author);
       if (!player)
-        return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setTimestamp().setDescription("Nothing is playing right now...")] });
+        return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setTimestamp().setDescription("沒有歌曲正在撥放..")] });
       if (res.loadType === 'LOAD_FAILED') {
         if (!player.queue.current) player.destroy();
         throw res.exception;
@@ -49,7 +49,7 @@ module.exports = {
     switch (res.loadType) {
       case 'NO_MATCHES':
         if (!player.queue.current) player.destroy();
-        return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setTimestamp().setDescription(`No matches found for - ${search}`)]});
+        return message.channel.send({ embeds: [new EmbedBuilder().setColor(client.embedColor).setTimestamp().setDescription(`找不到相應的 - ${search} 歌曲`)]});
       case 'TRACK_LOADED':
         var track = res.tracks[0];
         player.queue.add(track);
@@ -60,7 +60,7 @@ module.exports = {
             .setColor(client.embedColor)
             .setTimestamp()
             .setThumbnail(track.displayThumbnail("hqdefault"))
-            .setDescription(`${emojiaddsong} **Added song to queue**\n[${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\``)
+            .setDescription(`${emojiaddsong} **已新增歌曲至待播清單**\n[${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\``)
           return message.channel.send({ embeds: [thing] });
         }
       case 'PLAYLIST_LOADED':
@@ -69,7 +69,7 @@ module.exports = {
         const thing = new EmbedBuilder()
           .setColor(client.embedColor)
           .setTimestamp()
-          .setDescription(`${emojiplaylist} **Added playlist to queue**\n${res.tracks.length} Songs [${res.playlist.name}](${search}) - \`[${convertTime(res.playlist.duration)}]\``)
+          .setDescription(`${emojiplaylist} **已新增歌單至待播清單**\n${res.tracks.length} Songs [${res.playlist.name}](${search}) - \`[${convertTime(res.playlist.duration)}]\``)
         return message.channel.send({ embeds: [thing] });
       case 'SEARCH_RESULT':
         var track = res.tracks[0];
@@ -81,7 +81,7 @@ module.exports = {
             .setColor(client.embedColor)
             .setTimestamp()
             .setThumbnail(track.displayThumbnail("hqdefault"))
-            .setDescription(`${emojiaddsong} **Added song to queue**\n[${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\`[<@${track.requester.id}>]`)
+            .setDescription(`${emojiaddsong} **已新增歌曲至待播清單**\n[${track.title}](${track.uri}) - \`[${convertTime(track.duration)}]\`[<@${track.requester.id}>]`)
           return message.channel.send({ embeds: [thing] });
         }
     }
